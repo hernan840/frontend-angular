48cc5c83a9aff89c806bf29f0adeb94cad71331f
hernan840 <hernanflores21@gmail.com>
2023-01-18 17:55:16 -0300

    crear componente portada

--- frontend-angular/src/app/app.module.ts
+++ frontend-angular/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './componentes/menu/menu.component';
+import { PortadaComponent } from './componentes/portada/portada.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    PortadaComponent
   ],
   imports: [
     BrowserModule
