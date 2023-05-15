7c0f96a9619802aceb3e14297f737bcba5d37af1
hernan840 <hernanflores21@gmail.com>
2023-05-15 15:57:32 -0300

    muestra los datos del backend

--- frontend-angular/src/app/componentes/experiencias/experiencias.component.ts
+++ frontend-angular/src/app/componentes/experiencias/experiencias.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ExperienciaService } from 'src/app/servicios/experiencia.service';
 
 import { PorfolioService } from 'src/app/servicios/porfolio.service';
 
@@ -10,18 +11,35 @@ import { PorfolioService } from 'src/app/servicios/porfolio.service';
 })
 export class ExperienciasComponent implements OnInit{
 
-  experienciaLista: any;
+  //Variable para usarla(enlazarla) al template(en el html de la vista)
+  experiencias: any[]=[];
 
   // inyectamos el servicio en el contructor
-  constructor(private datosPorfolio:PorfolioService) { }
+  constructor(private experienciaService : ExperienciaService) { }
 
-  // para acceder al servicio lo hace a traves de ngOnInit
+  
   ngOnInit(): void {
-    this.datosPorfolio.obtenerDatos().subscribe(data => {
-      // console.log(data);
-      this.experienciaLista= data.experience;
-    });
-
+    this.cargarProyectos();
+   
   }
+
+  cargarProyectos():void{
+    this.experienciaService.lista().subscribe(data =>{
+      this.experiencias = data;
+    });
+  }
+
+  borrar(id:number){
+    if (id != undefined){
+      this.experienciaService.delete(id).subscribe(
+        data =>{
+          //alert("Halidad eliminada!!!");
+          this.cargarProyectos();
+        }, err =>{
+          //alert("No se pudo eliminar la habilidad!")
+          window.location.reload();
+        })
+    }}
+
 
 }
